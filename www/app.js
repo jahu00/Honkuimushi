5616abf8b792ff73f7e73b3c97872497d25714d5
jahu00 <jahu00@users.noreply.github.com>
2016-06-16 21:05:31 +0200

    Adjusting image size to screen size

--- www/app.js
+++ www/app.js
@@ -2,12 +2,40 @@ var App = function()
 {
 	var self = this;
 	self.container = $('#Container');
+	$(window).resize(function()
+	{
+		self.screenResize(true);
+	}).resize();
 	self.appState = "Loading";
 	self.switchScreen("loading");
 	self.dictionary = new rcxDict(false, null, function() {self.loadingUpdate() });
 	self.wordFrequency = new WordFrequency(function() {self.loadingUpdate() });
 }
 App.prototype = {
+	screenResize: function()
+	{
+		var _constants = constants;
+		var windowWidth = $(window).width();
+		var windowHeight = $(window).height();
+		var scale = windowWidth / _constants.baseWidth;
+		var projectedHeight = _constants.baseHeight * scale;
+		var projectedWidth = windowWidth;
+		var verticalOffset = parseInt((windowHeight - projectedHeight) / 2);
+		if (projectedHeight > windowHeight)
+		{
+			//Adjust to window height
+			var scale = windowHeight / _constants.baseHeight;
+			projectedHeight = windowHeight;
+			projectedWidth = scale * _constants.baseWidth;
+			verticalOffset = 0;
+		}
+		
+		// Adjust to window width
+		this.container.css("margin-top", verticalOffset + "px");
+		this.container.css("width", parseInt(projectedWidth) + "px");
+		this.container.css("height", parseInt(projectedHeight) + "px");
+		this.container.css("font-size", scale.toFixed(2) + "em")
+	},
 	switchScreen: function(name)
 	{
 		this.container.children('.screen').hide();
---
+++ www/constants.js
@@ -0,0 +1,7 @@
+var constants = {
+	width: 7,
+	height: 7,
+	minLength: 3,
+	baseWidth: 640,
+	baseHeight: 480
+}
